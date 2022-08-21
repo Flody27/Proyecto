3b68ce8e3f918b6dca68cb05ab0128ffbd6276a5
wretana1294 <98444957+wretana1294@users.noreply.github.com>
2022-08-21 11:12:08 -0600

    modificaciones aspecto Login

--- View/login.php
+++ View/login.php
@@ -10,36 +10,37 @@
 
   <title>CONVENIO MARCO - Inicio de Sesión</title>
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../css/login.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 
 <body>
-  <section class="vh-100 gradient-custom">
+  <section class="vh-100" style="background:#155db1;">
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card border-0 text-white" style="border-radius: 1rem; background: rgba(22,97,97,1.0) ">
+          <div class="card border-0 text-black" style="border-radius: 1rem; background: #f3c332">
             <div class="card-body p-5 text-center">
 
               <div class="mb-md-5 mt-md-4 pb-5">
                 
                 <form action="../Controller/Login-Controller.php" method="POST">
-                  <h2 class="fw-bold mb-2 text-uppercase">Inicio de Sesión</h2>
-                  <div class="form-outline form-white mb-4">
-                    <input type="text" id="username" name="username" class="form-control form-control-lg" />
+                <img src="../logo.png" alt="logo" style="height: 100px; margin-bottom: 20px;">
+                <h1 class="fw-bold mb-2 text-uppercase" style="padding-bottom: 15px;">Convenio Marco</h1>
+                  <h3 class="fw-bold mb-2 text-uppercase" style="padding-bottom: 15px;">Inicio de Sesión</h3>
+                  <div class="form-outline form-black mb-4">
+                    <input type="text" autocomplete="off" id="username" name="username" class="form-control form-control-lg" />
                     <label class="form-label" for="username">Nombre de Usuario</label>
                   </div>
 
-                  <div class="form-outline form-white mb-4">
+                  <div class="form-outline form-black mb-4">
                     <input type="password" name="password" id="contrasena" class="form-control form-control-lg" />
                     <label class="form-label" for="contrasena">Contraseña</label>
                   </div>
 
-                  <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="password-change.php">Olvidé mi contraseña.</a></p>
+                  <p class="small mb-5 pb-lg-2"><a class="text-black-50" href="password-change.php">Olvidé mi contraseña.</a></p>
 
-                  <button class="btn btn-outline-light btn-lg px-5" name="login-btn" type="submit">Ingresar</button>
+                  <button class="btn btn-outline-dark btn-md px-5" name="login-btn" type="submit">Ingresar</button>
                 </form>
 
               </div>
